d2fedf51633684240b82ea74911280a4d771c8f8
Sushant Giri <sushantgiri38@gmail.com>
2024-08-28 10:38:56 +0545

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sushant Giri on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
